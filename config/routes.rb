8a78550a2d4478ca92b1d116a73086e3181a5511
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2019-11-19 10:28:03 +0000

    added the final routeS

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:destroy] do
+  resources :users do
     resources :pets
   end
+
+  resources :pets do
+    resources :bookings
+  end
+
+  resources :bookings do
+    resources :reviews
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
