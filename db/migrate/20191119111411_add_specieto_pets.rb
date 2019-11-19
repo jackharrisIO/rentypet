83ed5f1f0e2cae4291de329f637775e50d2f1938
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2019-11-19 11:56:11 +0000

    please migrate database

---
+++ db/migrate/20191119111411_add_specieto_pets.rb
@@ -0,0 +1,4 @@
+class AddSpecietoPets < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_163228) do
+ActiveRecord::Schema.define(version: 2019_11_19_111539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,13 @@ ActiveRecord::Schema.define(version: 2019_11_18_163228) do
     t.string "personality"
     t.string "location"
     t.string "gender"
-    t.string "temprament"
+    t.string "temperament"
     t.boolean "child_friendly"
     t.string "guidelines"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "species", null: false
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
