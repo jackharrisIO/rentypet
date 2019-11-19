e2007058a7c63a125e09fe5d47e4a51eb10fc685
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2019-11-19 15:56:25 +0000

    fixed the bug, please pull asap

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :pets
   has_many :bookings
+
   validates :email, uniqueness: true, presence: true
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   has_many :booked_pets, through: :bookings, source: :pet
 end
