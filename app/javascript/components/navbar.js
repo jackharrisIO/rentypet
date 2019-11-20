7ce78b13590f3ee91a15abbae3e23fb149083fbc
TillyFawcett <tillyfawcett@hotmail.com>
2019-11-20 16:10:31 +0000

    changed some css

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,7 +3,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 800) {
         navbar.classList.add('navbar-lewagon-black');
       } else {
         navbar.classList.remove('navbar-lewagon-black');
