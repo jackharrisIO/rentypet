5e1bdcfc662af03f1eb38b68eacc9d2c6447657c
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2019-11-21 11:17:46 +0000

    added algolia

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
 gem 'faker'
+gem "algoliasearch-rails"
+gem 'compass'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,14 @@
 import "bootstrap";
+// import "places.js";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { initAutocomplete } from '../components/init_autocomplete';
+
+// import 'select2/dist/css/select2.css';
+
+// import { initSelect2 } from '../components/initselect2';
+// import { places } from '../packs/places';
 initUpdateNavbarOnScroll();
+initAutocomplete();
+
+// initSelect2();
+
