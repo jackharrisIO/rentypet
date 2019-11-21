40ea871279027f9e81db6678429c634daefe7d51
Jack Harris <jack@tallyho.cc>
2019-11-21 12:15:28 +0000

    changed fixed width and typedjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ initAutocomplete();
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 initMapbox();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
